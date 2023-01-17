4b0bfc786d2d92b4cf5305bc1791f26174dc2c6f
SanderOlsen98 <sanderlolsen@gmail.com>
2023-01-17 21:25:16 +0100

    update drivers info page

--- posten-website/src/components/dashboard/drivers/Drivers.js
+++ posten-website/src/components/dashboard/drivers/Drivers.js
@@ -23,7 +23,7 @@ function Dashboard() {
             <p>+4799999999</p>
           </button>
         </div>
-        <div className=" grid grid-rows-6 grid-flow-col gap-1 border-t py-3 border-b md:row md:flex-col">
+        <div className=" grid grid-rows-4 grid-flow-col gap-1 border-t py-3 border-b md:row md:flex-col">
           <div className=" py-2">
             <button
               id="Burhan"
@@ -33,7 +33,7 @@ function Dashboard() {
               }}
               className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32"
             >
-              Burhan
+              Knut
             </button>
           </div>
           <div className=" py-2">
@@ -45,7 +45,7 @@ function Dashboard() {
               }}
               className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32"
             >
-              Kjell A H
+              Espen
             </button>
           </div>
           <div className=" py-2">
@@ -57,7 +57,7 @@ function Dashboard() {
               }}
               className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32"
             >
-              Zoran
+              Alexandros
             </button>
           </div>
           <div className=" py-2">
@@ -69,7 +69,7 @@ function Dashboard() {
               }}
               className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32"
             >
-              Kitto
+              Lars
             </button>
           </div>
           <div className=" py-2">
@@ -81,7 +81,7 @@ function Dashboard() {
               }}
               className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32"
             >
-              Shaho
+              Andre
             </button>
           </div>
           <div className=" py-2">
@@ -93,7 +93,7 @@ function Dashboard() {
               }}
               className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32"
             >
-              Anne-Bente
+              Backer
             </button>
           </div>
           <div className=" py-2">
@@ -105,187 +105,7 @@ function Dashboard() {
               }}
               className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32"
             >
-              Hugo
-            </button>
-          </div>
-          <div className=" py-2">
-            <button
-              id="Steinar"
-              onClick={() => {
-                setButtonPopup(true);
-                setChangeColor("Steinar");
-              }}
-              className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32"
-            >
-              Steinar
-            </button>
-          </div>
-          <div className=" py-2">
-            <button
-              id="Hussain"
-              onClick={() => {
-                setButtonPopup(true);
-                setChangeColor("Hussain");
-              }}
-              className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32"
-            >
-              Hussain
-            </button>
-          </div>
-          <div className=" py-2">
-            <button
-              id="Waseem"
-              onClick={() => {
-                setButtonPopup(true);
-                setChangeColor("Waseem");
-              }}
-              className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32"
-            >
-              Waseem
-            </button>
-          </div>
-          <div className=" py-2">
-            <button
-              id="Saeed"
-              onClick={() => {
-                setButtonPopup(true);
-                setChangeColor("Saeed");
-              }}
-              className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32"
-            >
-              Saeed
-            </button>
-          </div>
-          <div className=" py-2">
-            <button
-              id="Kurt"
-              onClick={() => {
-                setButtonPopup(true);
-                setChangeColor("Kurt");
-              }}
-              className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32"
-            >
-              Kurt
-            </button>
-          </div>
-          <div className=" py-2">
-            <button
-              id="Tor"
-              onClick={() => {
-                setButtonPopup(true);
-                setChangeColor("Tor");
-              }}
-              className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32 sm:flex-row"
-            >
-              Tor
-            </button>
-          </div>
-          <div className=" py-2">
-            <button
-              id="Sebastian"
-              onClick={() => {
-                setButtonPopup(true);
-                setChangeColor("Sebastian");
-              }}
-              className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32"
-            >
-              Sebastian
-            </button>
-          </div>
-          <div className=" py-2">
-            <button
-              id="Sander"
-              onClick={() => {
-                setButtonPopup(true);
-                setChangeColor("Sander");
-              }}
-              className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32"
-            >
               Sander
-            </button>
-          </div>
-          <div className=" py-2">
-            <button
-              id="Steffen"
-              onClick={() => {
-                setButtonPopup(true);
-                setChangeColor("Steffen");
-              }}
-              className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32"
-            >
-              Steffen
-            </button>
-          </div>
-          <div className=" py-2">
-            <button
-              id="Emil"
-              onClick={() => {
-                setButtonPopup(true);
-                setChangeColor("Emil");
-              }}
-              className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32"
-            >
-              Emil
-            </button>
-          </div>
-          <div className=" py-2">
-            <button
-              id="Simon"
-              onClick={() => {
-                setButtonPopup(true);
-                setChangeColor("Simon");
-              }}
-              className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32"
-            >
-              Simon
-            </button>
-          </div>
-          <div className=" py-2">
-            <button
-              id="Roy B"
-              onClick={() => {
-                setButtonPopup(true);
-                setChangeColor("Roy B");
-              }}
-              className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32"
-            >
-              Roy B
-            </button>
-          </div>
-          <div className=" py-2">
-            <button
-              id="Erik"
-              onClick={() => {
-                setButtonPopup(true);
-                setChangeColor("Erik");
-              }}
-              className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32"
-            >
-              Erik
-            </button>
-          </div>
-          <div className=" py-2">
-            <button
-              id="Kjell A S"
-              onClick={() => {
-                setButtonPopup(true);
-                setChangeColor("Kjell A S");
-              }}
-              className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32"
-            >
-              Kjell A S
-            </button>
-          </div>
-          <div className=" py-2">
-            <button
-              id="Tobias"
-              onClick={() => {
-                setButtonPopup(true);
-                setChangeColor("Tobias");
-              }}
-              className="bg-gray-300 text-gray-900 font-bold px-4 rounded opacity-50 hover:opacity-30 py-4 w-32"
-            >
-              Tobias
             </button>
           </div>
         </div>
--- posten-website/src/components/drivers/Drivers.js
+++ posten-website/src/components/drivers/Drivers.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Drivers() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState("");
+
+  const handleButtonClick = (content) => {
+    setIsModalOpen(true);
+    setModalContent(content);
+  };
+
   return (
     <>
-      <div className=" mt-44">Drivers</div>
-      <div className=" mt-96"></div>;
+      <div className="flex justify-center items-center h-screen">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => handleButtonClick("Drivers Sentrum")}
+        >
+          Sentrum
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"
+          onClick={() => handleButtonClick("Drivers Fillipstad:")}
+        >
+          Fillipstad
+        </button>
+        {isModalOpen && (
+          <div className=" fixed top-[30%] right-[50%] bg-gray-900 opacity-75">
+            <div className="relative m-auto p-6 bg-white rounded-lg">
+              <p>{modalContent}</p>
+              <button onClick={() => setIsModalOpen(false)}>Close</button>
+            </div>
+          </div>
+        )}
+      </div>
     </>
   );
 }
